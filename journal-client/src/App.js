fd8b26b4d40677485f88c8a151ba5550dbf3e624
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-03-31 19:09:32 -0700

    single entry page structure

--- journal-client/src/App.js
+++ journal-client/src/App.js
@@ -2,14 +2,17 @@ import "./App.scss";
 import { Link, BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import Gallery from "./pages/Gallery/Gallery";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Switch>
+        {/* <Switch>
           <Route path="/" component={WelcomePage} />
-        </Switch>
+        </Switch> */}
+        <NavBar />
+        <Gallery />
       </BrowserRouter>
     </>
   );
