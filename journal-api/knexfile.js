ef372b0ddf4ef5f9ac6e130ae78eae20f3719131
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-06-13 15:09:57 -0700

    server config for production

--- journal-api/knexfile.js
+++ journal-api/knexfile.js
@@ -9,4 +9,26 @@ module.exports = {
       charset: "utf8",
     },
   },
+  production: {
+    client: "mysql",
+    connection: process.env.JAWSDB_URL,
+  },
 };
+
+// module.exports =
+//   process.env.NODE_ENV === "production"
+//     ? connections.production
+//     : connections.development;
+
+// module.exports = {
+//   development: {
+//     client: "mysql",
+//     connection: {
+//       host: "127.0.0.1",
+//       user: "root",
+//       password: "rootroot",
+//       database: "journal",
+//       charset: "utf8",
+//     },
+//   },
+// };
