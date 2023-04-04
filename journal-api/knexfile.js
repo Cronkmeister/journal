4ad7010f10db38afd033a08323eac49c5e8303ca
Jonny Cronk <jonnycronk@me.com>
2023-04-04 12:43:40 -0700

    deploy test2

--- journal-api/knexfile.js
+++ journal-api/knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
     client: "mysql2",
     connection: {
       host: process.env.AWS_CONNECTION_ENDPOINT,
-      // port: 3306,
+      port: "3306",
       user: process.env.AWS_DB_USERNAME,
       password: process.env.AWS_DB_PASSWORD,
       database: "journal",
